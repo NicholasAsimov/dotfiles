84b5181604d9bd3b8d8cf131c41fb479ad185856
Nicholas Asimov <nicholas@asimov.me>
2017-05-11 04:48:21 +0300

    Add Arch-specific installation script
    Currently it only installs yaourt.

---
+++ arch-setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+function install_yaourt() {
+    git clone https://aur.archlinux.org/package-query.git
+    cd package-query
+    makepkg -si
+    cd ..
+    git clone https://aur.archlinux.org/yaourt.git
+    cd yaourt
+    makepkg -si
+    cd ..
+}
+
+echo "Installing yaourt"
+install_yaourt
