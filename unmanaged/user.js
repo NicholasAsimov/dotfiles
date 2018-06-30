3552eb9cb018238adfea4cd837654da05ec29c47
Nicholas Asimov <nicholas@asimov.me>
2018-06-30 17:06:24 +0300

    Add firefox preference file

---
+++ unmanaged/user.js
@@ -0,0 +1,23 @@
+// Disable alt-key opening menu bar (interferes with media keybinds e.g. Alt+3)
+user_pref("ui.key.menuAccessKeyFocuses", false);
+
+// Don't hide tab bar in fullscreen
+user_pref("browser.fullscreen.autohide", false);
+
+// Make URL bar behaviour similar to Chrome
+user_pref("browser.urlbar.clickSelectsAll", true);
+user_pref("browser.urlbar.doubleClickSelectsAll", false);
+
+// Font settings
+user_pref("font.default.x-cyrillic", "sans-serif");
+user_pref("font.default.x-western", "sans-serif");
+user_pref("font.name.monospace.x-cyrillic", "Fira Mono");
+user_pref("font.name.monospace.x-western", "Fira Mono");
+user_pref("font.name.sans-serif.x-cyrillic", "Droid Sans");
+user_pref("font.name.sans-serif.x-western", "Droid Sans");
+user_pref("font.name.serif.x-cyrillic", "Droid Serif");
+user_pref("font.name.serif.x-western", "Droid Serif");
+user_pref("font.size.variable.x-cyrillic", 16);
+user_pref("font.size.variable.x-western", 16);
+user_pref("font.size.fixed.x-cyrillic", 13);
+user_pref("font.size.fixed.x-western", 13);
