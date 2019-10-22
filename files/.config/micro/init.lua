0d1e6dc6c42bed61fd9f6ef9b2f0903847ee825d
Nicholas Asimov <nicholas@asimov.me>
2019-10-22 14:04:50 +0300

    Remove unused lilyterm and micro config files

--- files/.config/micro/init.lua
+++
@@ -1,6 +0,0 @@
-function gorun()
-    local buf = CurView().Buf -- The current buffer
-    if buf:FileType() == "go" then
-        HandleShellCommand("go run " .. buf.Path, true, true) -- the first true means don't run it in the background
-    end
-end
